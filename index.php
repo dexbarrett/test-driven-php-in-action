3465b060c49fe0315b5fa6686547bc0987d074ee
Dex Barrett <dexter.x740@gmail.com>
2013-07-21 19:36:18 -0700

    added index file to test Logger in action

--- index.php
+++ index.php
@@ -2,4 +2,14 @@
 
 require_once './vendor/autoload.php';
 
-$logger = new \Enlog\Logger();
+use \Enlog\Logger;
+use \Enlog\Handler\MemoryHandler;
+
+$logger  = new Logger();
+$handler = new MemoryHandler();
+
+$logger->registerHandler('memory', $handler);
+
+$logger->log("Yay, I wrote a simple library using TDD!");
+
+var_dump($handler->getEntries());
