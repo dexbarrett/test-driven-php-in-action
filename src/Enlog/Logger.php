fe4a4434cc098e5934cb3c392d4099e77596e5f9
Dex Barrett <dexter.x740@gmail.com>
2013-07-21 19:31:11 -0700

    added write method

--- src/Enlog/Logger.php
+++ src/Enlog/Logger.php
@@ -1,7 +1,28 @@
 <?php
 namespace Enlog;
 
-class Logger
-{
+use \Enlog\Handler\HandlerInterface;
 
+class Logger
+{    
+    protected $handlers = array();
+
+    public function registerHandler($name, HandlerInterface $handlers)
+    {
+        $this->handlers[$name] = $handlers;
+    }
+
+    public function getHandler($name)
+    {
+        return $this->handlers[$name];
+    }
+
+    public function log($message)
+    {
+        foreach($this->handlers as $handler)
+        {
+            $date = date('r');
+            $handler->write($date, $message);
+        }    
+    }
 }
--- tests/Enlog/LoggerTest.php
+++ tests/Enlog/LoggerTest.php
@@ -12,5 +12,43 @@ class LoggerTest extends PHPUnit_Framework_TestCase
     public function testInstance()
     {
         $this->assertInstanceOf('\Enlog\Logger', $this->logger);
-    }     
+    }
+
+    public function testRegistersHandlerCorrectly()
+    {
+       $handler = $this->getHandlerMock();
+       $this->logger->registerHandler('memory', $handler);
+       $this->assertSame($handler, $this->logger->getHandler('memory'));
+    }
+
+    public function testRegistersMoreThanOneHandlerCorrectly()
+    {
+       $handler  = $this->getHandlerMock();
+       $handler2 = $this->getHandlerMock();
+       $this->logger->registerHandler('memory', $handler);
+       $this->logger->registerHandler('memory2', $handler2);
+       $this->assertSame($handler, $this->logger->getHandler('memory'));
+       $this->assertSame($handler2, $this->logger->getHandler('memory2'));
+
+    }
+
+    public function testPassingCallsToHandlers()
+    {
+        $mock = $this->getHandlerMock();
+        $mock->expects($this->exactly(2))
+             ->method('write')
+             ->with(
+                 $this->isType('string'),
+                 $this->equalTo('Hello') 
+              );
+
+        $this->logger->registerHandler('memory', $mock);
+        $this->logger->registerHandler('memory2', $mock);
+        $this->logger->log('Hello');    
+    }
+
+    protected function getHandlerMock()
+    {
+        return $this->getMock('\Enlog\Handler\HandlerInterface');    
+    }        
 }
